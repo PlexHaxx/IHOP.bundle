66b4460a284f9fa0cee83b0bfdf9b40788a30585
Justin Unwin <junwin@gmail.com>
2015-01-03 09:26:59 +0200

    Tweaking the code so to load the worship leaders from the javascript

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -7,7 +7,7 @@ MAXRESULTS = 50
 IHOP_FEED_URL = "http://feed.theplatform.com/f/IfSiAC/5ct7EYhhJs9Z/"
 IHOP_FEED_QUERY = "?q=%s&range=0-%s&=&sort=pubDate|desc&count=true"
 IHOP_FEED_FILT = "&byCustomValue={ihopkc$setType}{Intercession}"
-IHOP_JAVASCRIPT = "http://cdn.ihopkc.org.edgesuite.net/wp-content/themes/ihopkc_theme/_js/ihopkc-main.min.js"
+IHOP_JAVASCRIPT_URL = "http://cdn.ihopkc.org.edgesuite.net/wp-content/themes/ihopkc_theme/_js/ihopkc-main.min.js"
 
 RE_WLEADER = Regex("""e.worshipLeaders\[0\]\.toLowerCase\(\)\)return"";var t\=(\[[a-zA-Z0-9\a\A\s",]+\]);""")
 
@@ -27,7 +27,8 @@ def MainMenu():
     wleaders = []
     try:
         Log.Debug("Loading Worship Leaders")
-        wleaders = JSON.ObjectFromString(RE_WLEADER.search(IHOP_JAVASCRIPT).group(0))
+        IHOP_JAVASCRIPT_RAW = HTTP.Request(IHOP_JAVASCRIPT_URL).content
+        wleaders = JSON.ObjectFromString(RE_WLEADER.search(IHOP_JAVASCRIPT_RAW).group(0))
         Log.Debug(str(wleaders))
     except:
         pass
